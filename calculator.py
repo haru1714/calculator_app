0f2d4d4e1f91e6d07b572a3cc3d0cf537a1a13d7
a3265028 <a3265028@mi.sanno.ac.jp>
2026-05-09 15:05:17 +0900

    update:入力のための式作成関数、演算子の入力接続関数作成、数値入力関数の式作成関数経由化

--- calculator.py
+++ calculator.py
@@ -14,9 +14,11 @@ def update_number(now_number,on_number): #入力数値を並べる関数
     return now_number
 
 def operator_input(on_operator,pre_operator,pre_number,now_number): #演算子の入力関数
+
     if pre_operator == 0:
         pre_operator = on_operator
         pre_number = now_number
+        now_number = 0
     
     else:
         if pre_operator == 1:
@@ -38,11 +40,40 @@ def exe_equal(pre_operator,pre_number,now_number): #イコール処理
     pre_number = 0
     return(pre_operator,pre_number,now_number)  
 
-def number_click(number): #update_number()とupdate_display()の接続
+def number_click(number): #数字のクリック関数
     global now_number
+    global pre_operator
+    global pre_number
+
     now_number = update_number(now_number,number)
-    update_display(now_number)
-    return(now_number)
+    formula = build_display(pre_operator,pre_number,now_number)
+    update_display(formula)
+
+def operator_click(number):
+    global pre_operator
+    global pre_number
+    global now_number
+
+    pre_operator,pre_number,now_number = operator_input(number,pre_operator,pre_number,now_number)
+    formula = build_display(pre_operator,pre_number,now_number)
+    update_display(formula)
+
+
+
+def build_display(pre_operator,pre_number,now_number): #式作成関数
+    if pre_operator == 0:
+        formula = str(now_number)
+    elif pre_operator == 1 and now_number == 0:
+        formula = str(pre_number) + "+"
+    elif pre_operator == 2 and now_number == 0:
+        formula = str(pre_number) + "-"
+    elif pre_operator == 1 and now_number != 0:
+        formula = str(pre_number) + "+" + str(now_number)
+    elif pre_operator == 2 and now_number != 0:
+        formula = str(pre_number) + "-" + str(now_number)
+
+    return(formula)
+
 
 def update_display(now_number): #Tkinterへの反映
     entry.config(state="normal")
@@ -73,8 +104,8 @@ btn_7 = tk.Button(root,text="7",width="5",font=("Arial",30),activebackground="gr
 btn_8 = tk.Button(root,text="8",width="5",font=("Arial",30),activebackground="gray",command=lambda:number_click(8))
 btn_9 = tk.Button(root,text="9",width="5",font=("Arial",30),activebackground="gray",command=lambda:number_click(9))
 btn_0 = tk.Button(root,text="0",width="5",font=("Arial",30),activebackground="gray",command=lambda:number_click(0))
-btn_plus = tk.Button(root,text="+",width="5",font=("Arial",30),activebackground="gray",command=lambda:operator_input(1))
-btn_minus = tk.Button(root,text="-",width="5",font=("Arial",30),activebackground="gray",command=lambda:operator_input(2))
+btn_plus = tk.Button(root,text="+",width="5",font=("Arial",30),activebackground="gray",command=lambda:operator_click(1))
+btn_minus = tk.Button(root,text="-",width="5",font=("Arial",30),activebackground="gray",command=lambda:operator_click(2))
 btn_equal = tk.Button(root,text="=")
 
 entry.grid(row=0,column=0,columnspan=3)
